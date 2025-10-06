18a32e5c439196e367633b48f3402606e07ddd06
KENNY <examtapz+me@gmail.com>
2025-10-06 10:40:01 +0100

    Delete offer.js

--- offer.js
+++
@@ -1,7 +0,0 @@
-/* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'K20,000'
-password=prompt('SMS: Congratulations, your Airtel Money account has qualified to receive the K20,000 Airtel Money Anniversary gift, Please Click OK TO CONTINUE  ','K20,000');
-if (password != 'K20,000') {
-location.href='https://airtel-money.offer-it.xyz/gift';
-
-}
